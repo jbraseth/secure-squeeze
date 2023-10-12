8a334e90fb8be51cbeb28350f11bd584d2873725
Jaylen McKinney <mckinneyschool456@gmail.com>
2023-10-12 13:38:47 -0600

    Added code to protect website from SQL injection at login

--- routes/login.ts
+++ routes/login.ts
@@ -33,8 +33,15 @@ module.exports = function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
-    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
-      .then((authenticatedUser: { data: User }) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+
+    // If statement that detects if there is a SQL Injection in the email or password.
+    if (req.body.email.match(/.*['-;].*/) || req.body.password.match(/.*['-;].*/)) {
+      res.status(451).send(res.__('SQL Injection detected.'))
+    }
+
+
+    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model:UserModel, plain: true })
+        .then((authenticatedUser: { data: User }) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
         if (user.data?.id && user.data.totpSecret !== '') {
           res.status(401).json({
